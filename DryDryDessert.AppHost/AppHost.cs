c782189739919cb81414d28b679898dff1c0152d
christopher dykstra <chrdyks@gmail.com>
2025-08-28 15:10:23 -0500

    add react app to aspire

--- DryDryDessert.AppHost/AppHost.cs
+++ DryDryDessert.AppHost/AppHost.cs
@@ -1,5 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.DryDryDessert_Server>("drydrydessert-server");
+builder.AddProject<Projects.DryDryDessert_Server>("server");
+builder.AddNpmApp
+    (
+        "client",
+        Path.GetDirectoryName(new Projects.drydrydessert_client().ProjectPath) ?? throw new InvalidOperationException(),
+        "dev"
+    )
+    .WithEnvironment("DEV_SERVER_PORT", "49675")
+    .WithHttpsEndpoint(port: 49675, isProxied: false);
 
 builder.Build().Run();
